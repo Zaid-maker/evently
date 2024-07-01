eb79211a69ae3fc4f0e86668c69e50ecf1c9e0c1
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-07-01 17:15:23 +0000

    Footer

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,4 @@
-// import Footer from "@/components/shared/Footer"
+import Footer from "@/components/shared/Footer";
 // import Header from "@/components/shared/Header"
 
 export default function RootLayout({
@@ -10,7 +10,7 @@ export default function RootLayout({
     <div className="flex h-screen flex-col">
       {/* <Header /> */}
       <main className="flex-1">{children}</main>
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
