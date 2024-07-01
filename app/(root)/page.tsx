7ea01d638685d0e85d642cb270917bdd98af35fe
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-07-01 11:21:04 +0000

    Update home

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,21 @@
 import React from "react";
 
 export default async function Home() {
-  return <div>page</div>;
+  return (
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
+        <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
+          <div className="flex flex-col justify-center gap-8">
+            <h1 className="h1-bold">
+              Host, Connect, Celebrate: Your Events, Our Platform!
+            </h1>
+            <p className="p-regular-20 md:p-regular-24">
+              Book and learn helpful tips from 3,168+ mentors in world-class
+              companies with our global community.
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
