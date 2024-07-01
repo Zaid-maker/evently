95cd16201925fe31df9c36c46018c72904b99b0f
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-07-01 17:21:29 +0000

    update Header

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,7 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image
+            src="/assets/images/logo.svg"
+            width={128}
+            height={38}
+            alt="Evently logo"
+          />
+        </Link>
+
+        <nav className="md:flex-between hidden w-full max-w-xs">NavItems</nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
