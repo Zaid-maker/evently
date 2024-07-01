dd8eb9eb2ad43584bd243279d4dff2879d25ef33
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-07-01 11:29:07 +0000

    more

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -29,6 +29,21 @@ export default async function Home() {
           />
         </div>
       </section>
+
+      <section
+        id="events"
+        className="wrapper my-8 flex flex-col gap-8 md:gap-12"
+      >
+        <h2 className="h2-bold">
+          Trust by <br /> Thousands of Events
+        </h2>
+
+        <div className="flex w-full flex-col gap-5 md:flex-row">
+          {/* <Search />
+          <CategoryFilter /> */}
+          Search CategoryFilter
+        </div>
+      </section>
     </>
   );
 }
