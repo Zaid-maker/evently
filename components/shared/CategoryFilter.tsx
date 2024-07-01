fbff0322f68aeaacbc393fe619c558c2d584c753
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-07-01 17:06:37 +0000

    Update CategoryFilter compoent

--- components/shared/CategoryFilter.tsx
+++ components/shared/CategoryFilter.tsx
@@ -1,7 +1,27 @@
+"use client";
+
 import React from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
 
 const CategoryFilter = () => {
-  return <div>CategoryFilter</div>;
+  return (
+    <Select>
+      <SelectTrigger className="select-field">
+        <SelectValue placeholder="Category" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="All" className="select-item p-regular-14">
+          All
+        </SelectItem>
+      </SelectContent>
+    </Select>
+  );
 };
 
 export default CategoryFilter;
