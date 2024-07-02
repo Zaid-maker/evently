7215834c7de0ec4c7124404b5318d48806494743
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-07-02 12:01:29 +0000

    re-organize imports

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,11 +1,14 @@
 "use server";
 
-import { CreateUserParams, UpdateUserParams } from "@/types";
+import { revalidatePath } from "next/cache";
+
 import { connectToDatabase } from "@/lib/database";
 import User from "@/lib/database/models/user.model";
+// import Order from '@/lib/database/models/order.model'
+import Event from "@/lib/database/models/event.model";
 import { handleError } from "@/lib/utils";
-import Event from "../database/models/event.model";
-import { revalidatePath } from "next/cache";
+
+import { CreateUserParams, UpdateUserParams } from "@/types";
 
 export async function createUser(user: CreateUserParams) {
   try {
