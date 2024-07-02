a438386cd8352e3e5a62a71ff7d6a5078ade5631
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-07-02 12:35:36 +0000

    update user action

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -22,6 +22,19 @@ export async function createUser(user: CreateUserParams) {
   }
 }
 
+export async function getUserById(userId: string) {
+  try {
+    await connectToDatabase();
+
+    const user = await User.findById(userId);
+
+    if (!user) throw new Error("User not found");
+    return JSON.parse(JSON.stringify(user));
+  } catch (error) {
+    handleError(error);
+  }
+}
+
 export async function updateUser(clerkId: string, user: UpdateUserParams) {
   try {
     await connectToDatabase();
@@ -42,26 +55,33 @@ export async function deleteUser(clerkId: string) {
   try {
     await connectToDatabase();
 
+    // Find user to delete
     const userToDelete = await User.findOne({ clerkId });
 
     if (!userToDelete) {
       throw new Error("User not found");
     }
 
+    // Unlink relationships
     await Promise.all([
+      // Update the 'events' collection to remove references to the user
       Event.updateMany(
         { _id: { $in: userToDelete.events } },
         { $pull: { organizer: userToDelete._id } },
       ),
 
-      Order.updateMany({ _id: { $in: userToDelete.orders } }),
-      { $unset: { buyer: 1 } },
+      // Update the 'orders' collection to remove references to the user
+      Order.updateMany(
+        { _id: { $in: userToDelete.orders } },
+        { $unset: { buyer: 1 } },
+      ),
     ]);
 
-    const deleteUser = await User.findByIdAndDelete(userToDelete._id);
+    // Delete user
+    const deletedUser = await User.findByIdAndDelete(userToDelete._id);
     revalidatePath("/");
 
-    return deleteUser ? JSON.parse(JSON.stringify(deleteUser)) : null;
+    return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null;
   } catch (error) {
     handleError(error);
   }
