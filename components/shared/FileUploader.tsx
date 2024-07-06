3f374411a6013d136db1ae8bd6611449aa160af8
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-07-06 09:36:57 +0000

    update

--- components/shared/FileUploader.tsx
+++ components/shared/FileUploader.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import React, { Dispatch, SetStateAction } from "react";
+import { Button } from "../ui/button";
+import Image from "next/image";
 
 type FileUploaderProps = {
   onFieldChange: (url: string) => void;
@@ -9,7 +11,23 @@ type FileUploaderProps = {
 };
 
 const FileUploader = ({}: FileUploaderProps) => {
-  return <div>FileUploader</div>;
+  return (
+    <div className="flex-center bg-dark-3 flex h-72 cursor-pointer overflow-hidden rounded-xl bg-gray-50">
+      <div className="flex-center flex-col py-5 text-grey-500">
+        <Image
+          src="/assets/icons/upload.svg"
+          width={77}
+          height={77}
+          alt="file upload"
+        />
+        <h3 className="mb-2 mt-2">Drag photo here</h3>
+        <p className="p-medium-12 mb-4">SVG, PNG, JPG</p>
+        <Button type="button" className="rounded-full">
+          Select from computer
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default FileUploader;
