722a50c4a919101909d922fdf2b5713715e7e79e
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-07-09 18:53:50 +0000

    EventDetails Page

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,7 +1,50 @@
+import { getEventById } from "@/lib/actions/event.actions";
+import { SearchParamProps } from "@/types";
+import Image from "next/image";
 import React from "react";
 
-const EventDetails = () => {
-  return <div>EventDetails</div>;
+const EventDetails = async ({
+  params: { id },
+  searchParams,
+}: SearchParamProps) => {
+  const event = await getEventById(id);
+
+  return (
+    <>
+      <section className="flex justify-center bg-primary-50 bg-dotted-pattern bg-contain">
+        <div>
+          <Image
+            src={event.imageUrl}
+            alt="hero image"
+            width={1000}
+            height={1000}
+            className="h-full min-h-[300px] object-cover object-center"
+          />
+          <div className="flex w-full flex-col gap-8 p-5 md:p-10">
+            <div className="flex flex-col gap-6">
+              <h2 className="h2-bold">{event.title}</h2>
+              <div className="flex flex-col gap-3 sm:flex-row sm:items-center">
+                <div className="flex gap-3">
+                  <p className="p-bold-20 rounded-full bg-green-500/10 px-5 py-2 text-green-700">
+                    {event.isFree ? "FREE" : `$${event.price}`}
+                  </p>
+                  <p className="p-medium-16 rounded-full bg-grey-500/10 px-4 py-2.5 text-grey-500">
+                    {event.category.name}
+                  </p>
+                </div>
+                <p className="p-medium-18 ml-2 mt-2 sm:mt-0">
+                  by{" "}
+                  <span className="text-primary-500">
+                    {event.organizer.firstName} {event.organizer.lastName}
+                  </span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default EventDetails;
