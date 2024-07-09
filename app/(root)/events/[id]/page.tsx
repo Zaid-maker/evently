529dcb32dbf06c0b78bae81e57be7e9b1c5dc1c8
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-07-09 18:54:58 +0000

    fix styling

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -12,7 +12,7 @@ const EventDetails = async ({
   return (
     <>
       <section className="flex justify-center bg-primary-50 bg-dotted-pattern bg-contain">
-        <div>
+        <div className="grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl">
           <Image
             src={event.imageUrl}
             alt="hero image"
