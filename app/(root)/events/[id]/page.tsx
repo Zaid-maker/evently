e6d2445b8ed41100ca15e03240a1b81f1e10af4b
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-07-10 16:44:15 +0000

    update

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,7 +1,12 @@
-import { getEventById } from "@/lib/actions/event.actions";
+// import CheckoutButton from '@/components/shared/CheckoutButton';
+// import Collection from '@/components/shared/Collection';
+import {
+  getEventById,
+  getRelatedEventsByCategory,
+} from "@/lib/actions/event.actions";
+import { formatDateTime } from "@/lib/utils";
 import { SearchParamProps } from "@/types";
 import Image from "next/image";
-import React from "react";
 
 const EventDetails = async ({
   params: { id },
@@ -39,6 +44,44 @@ const EventDetails = async ({
                   </span>
                 </p>
               </div>
+            </div>
+            CheckoutButton
+            <div className="flex flex-col gap-5">
+              <div className="flex gap-2 md:gap-3">
+                <Image
+                  src="/assets/icons/calendar.svg"
+                  alt="calendar"
+                  width={32}
+                  height={32}
+                />
+                <div className="p-medium-16 lg:p-regular-20 flex flex-wrap items-center">
+                  <p>
+                    {formatDateTime(event.startDateTime).dateOnly} -{" "}
+                    {formatDateTime(event.startDateTime).timeOnly}
+                  </p>
+                  <p>
+                    {formatDateTime(event.endDateTime).dateOnly} -{" "}
+                    {formatDateTime(event.endDateTime).timeOnly}
+                  </p>
+                </div>
+              </div>
+
+              <div className="p-regular-20 flex items-center gap-3">
+                <Image
+                  src="/assets/icons/location.svg"
+                  alt="location"
+                  width={32}
+                  height={32}
+                />
+                <p className="p-medium-16 lg:p-regular-20">{event.location}</p>
+              </div>
+            </div>
+            <div className="flex flex-col gap-2">
+              <p className="p-bold-20 text-grey-600">What You&apos;ll Learn:</p>
+              <p className="p-medium-16 lg:p-regular-18">{event.description}</p>
+              <p className="p-medium-16 lg:p-regular-18 truncate text-primary-500 underline">
+                {event.url}
+              </p>
             </div>
           </div>
         </div>
