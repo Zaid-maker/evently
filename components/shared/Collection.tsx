37b2ef3a3589de2b5739055540087a500c46c7c1
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-07-11 17:02:48 +0000

    pagination

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -35,6 +35,10 @@ const Collection = ({
               );
             })}
           </ul>
+
+          {/* {totalPages > 1 && (
+            <Pagination urlParamName={urlParamName} page={page} totalPages={totalPages} />
+          )} */}
         </div>
       ) : (
         <div className="flex-center wrapper min-h-[200px] w-full flex-col gap-3 rounded-[14px] bg-grey-50 py-28 text-center">
