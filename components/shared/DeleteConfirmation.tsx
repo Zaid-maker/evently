433854d98c6735f6a2b31a67700603f5b0c34fe7
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-07-12 14:44:55 +0000

    Update

--- components/shared/DeleteConfirmation.tsx
+++ components/shared/DeleteConfirmation.tsx
@@ -19,7 +19,31 @@ import {
 import { deleteEvent } from "@/lib/actions/event.actions";
 
 const DeleteConfirmation = ({ eventId }: { eventId: string }) => {
-  return <div>DeleteConfirmation</div>;
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger>
+        <Image
+          src="/assets/icons/delete.svg"
+          alt="edit"
+          width={20}
+          height={20}
+        />
+      </AlertDialogTrigger>
+
+      <AlertDialogContent className="bg-white">
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you sure you want to delete?</AlertDialogTitle>
+          <AlertDialogDescription className="p-regular-16 text-grey-600">
+            This will permanently delete this event
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
 };
 
 export default DeleteConfirmation;
