8494f6be159a91d54bd014a12238768550db49fa
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-07-12 12:19:34 +0000

    hidePrice

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -1,5 +1,6 @@
 import { IEvent } from "@/lib/database/models/event.model";
 import { formatDateTime } from "@/lib/utils";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -9,7 +10,7 @@ type CardProps = {
   hidePrice?: boolean;
 };
 
-const Card = ({ event }: CardProps) => {
+const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
   return (
     <div className="group relative flex min-h-[380px] w-full max-w-[400px] flex-col overflow-hidden rounded-xl bg-white shadow-md transition-all hover:shadow-lg md:min-h-[438px]">
       <Link
@@ -19,12 +20,18 @@ const Card = ({ event }: CardProps) => {
       />
 
       <div className="flex min-h-[230px] flex-col gap-3 p-5 md:gap-4">
-        <span className="p-semibold-14 w-min rounded-full bg-green-100 px-4 py-1 text-green-60">
-          {event.isFree ? "FREE" : `$${event.price}`}
-        </span>
-        <p className="p-semibold-14 w-min rounded-full bg-grey-500/10 px-4 py-1 text-grey-500 line-clamp-1">
-          {event.category.name}
-        </p>
+        {!hidePrice && (
+          <div>
+            <span className="p-semibold-14 w-min rounded-full bg-green-100 px-4 py-1 text-green-60">
+              {event.isFree ? "FREE" : `$${event.price}`}
+            </span>
+
+            <p className="p-semibold-14 w-min rounded-full bg-grey-500/10 px-4 py-1 text-grey-500 line-clamp-1">
+              {event.category.name}
+            </p>
+          </div>
+        )}
+
         <p className="p-medium-16 p-medium-18 text-grey-500">
           {formatDateTime(event.startDateTime).dateTime}
         </p>
@@ -39,6 +46,18 @@ const Card = ({ event }: CardProps) => {
           <p className="p-medium-14 md:p-medium-16 text-grey-600">
             {event.organizer.firstName} {event.organizer.lastName}
           </p>
+
+          {hasOrderLink && (
+            <Link href={`/orders?eventId=${event._id}`} className="flex gap-2">
+              <p className="text-primary-500">Order Details</p>
+              <Image
+                src="/assets/icons/arrow.svg"
+                alt="search"
+                width={10}
+                height={10}
+              />
+            </Link>
+          )}
         </div>
       </div>
     </div>
