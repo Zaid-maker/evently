72275a1249aae341e7ad61d30c998434864b9958
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-07-12 18:18:53 +0000

    Search placeholder

--- app/(root)/orders/page.tsx
+++ app/(root)/orders/page.tsx
@@ -1,3 +1,4 @@
+import Search from "@/components/shared/Search";
 import React from "react";
 
 const Orders = () => {
@@ -5,6 +6,10 @@ const Orders = () => {
     <>
       <section className=" bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <h3 className="wrapper h3-bold text-center sm:text-left ">Orders</h3>
+      </section>
+
+      <section className="wrapper mt-8">
+        <Search placeholder="Search buyer name..." />
       </section>
     </>
   );
--- components/shared/Search.tsx
+++ components/shared/Search.tsx
@@ -4,7 +4,11 @@ import Image from "next/image";
 import React from "react";
 import { Input } from "../ui/input";
 
-const Search = () => {
+const Search = ({
+  placeholder = "Search title...",
+}: {
+  placeholder?: string;
+}) => {
   return (
     <div className="flex-center min-h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
       <Image
