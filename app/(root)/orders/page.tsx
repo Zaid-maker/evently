8918e4841f590b5f4bd926d5940c0c24c5d6c652
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-07-12 18:16:37 +0000

    Orders Details Page

---
+++ app/(root)/orders/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Orders = () => {
+  return (
+    <>
+      <section className=" bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+        <h3 className="wrapper h3-bold text-center sm:text-left ">Orders</h3>
+      </section>
+    </>
+  );
+};
+
+export default Orders;
