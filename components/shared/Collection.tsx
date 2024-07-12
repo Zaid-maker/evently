eec25abc579eec80eec52af36a9ab771d1ad0b5f
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-07-12 09:09:42 +0000

    add hasOrderLink and hidePrize

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -28,9 +28,16 @@ const Collection = ({
         <div className="flex flex-col items-center gap-10">
           <ul className="grid w-full grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:gap-10">
             {data.map((event) => {
+              const hasOrderLink = collectionType === "Events_Organized";
+              const hidePrice = collectionType === "My_Tickets";
+
               return (
                 <li key={event._id} className="flex justify-center">
-                  <Card event={event} />
+                  <Card
+                    event={event}
+                    hasOrderLink={hasOrderLink}
+                    hidePrice={hidePrice}
+                  />
                 </li>
               );
             })}
