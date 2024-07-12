33608b7aa59cad62b40029f51916a5f30a315c38
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-07-12 18:23:01 +0000

    basic

--- app/(root)/orders/page.tsx
+++ app/(root)/orders/page.tsx
@@ -1,7 +1,15 @@
 import Search from "@/components/shared/Search";
-import React from "react";
+// import { getOrdersByEvent } from '@/lib/actions/order.actions'
+import { formatDateTime, formatPrice } from "@/lib/utils";
+import { SearchParamProps } from "@/types";
+import { IOrderItem } from "@/lib/database/models/order.model";
 
-const Orders = () => {
+const Orders = async ({ searchParams }: SearchParamProps) => {
+  const eventId = (searchParams?.eventId as string) || "";
+  const searchText = (searchParams?.query as string) || "";
+
+  //   const orders = await getOrdersByEvent({ eventId, searchString: searchText })
+
   return (
     <>
       <section className=" bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
@@ -10,6 +18,22 @@ const Orders = () => {
 
       <section className="wrapper mt-8">
         <Search placeholder="Search buyer name..." />
+      </section>
+
+      <section className="wrapper overflow-x-auto">
+        <table className="w-full border-collapse border-t">
+          <thead>
+            <tr className="p-medium-14 border-b text-grey-500">
+              <th className="min-w-[250px] py-3 text-left">Order ID</th>
+              <th className="min-w-[200px] flex-1 py-3 pr-4 text-left">
+                Event Title
+              </th>
+              <th className="min-w-[150px] py-3 text-left">Buyer</th>
+              <th className="min-w-[100px] py-3 text-left">Created</th>
+              <th className="min-w-[100px] py-3 text-right">Amount</th>
+            </tr>
+          </thead>
+        </table>
       </section>
     </>
   );
