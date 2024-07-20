613534ce1b09d5b08d55bfc03221c8f482df07f3
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-07-20 13:20:20 +0000

    Fix

--- app/(root)/orders/page.tsx
+++ app/(root)/orders/page.tsx
@@ -1,5 +1,5 @@
 import Search from "@/components/shared/Search";
-// import { getOrdersByEvent } from '@/lib/actions/order.actions'
+import { getOrdersByEvent } from "@/lib/actions/order.actions";
 import { formatDateTime, formatPrice } from "@/lib/utils";
 import { SearchParamProps } from "@/types";
 import { IOrderItem } from "@/lib/database/models/order.model";
@@ -8,7 +8,7 @@ const Orders = async ({ searchParams }: SearchParamProps) => {
   const eventId = (searchParams?.eventId as string) || "";
   const searchText = (searchParams?.query as string) || "";
 
-  //   const orders = await getOrdersByEvent({ eventId, searchString: searchText })
+  const orders = await getOrdersByEvent({ eventId, searchString: searchText });
 
   return (
     <>
