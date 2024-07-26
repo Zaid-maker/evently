e005fae46a5013026e5dadb83526adeba488a5e7
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-07-26 17:20:55 +0000

    update CheckoutButton

--- components/shared/CheckoutButton.tsx
+++ components/shared/CheckoutButton.tsx
@@ -1,7 +1,25 @@
+"use client";
+
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import React from "react";
+import { Button } from "../ui/button";
+import Link from "next/link";
 
 const CheckoutButton = () => {
-  return <div>CheckoutButton</div>;
+  return (
+    <div className="flex items-center gap-3">
+      <SignedOut>
+        <Button asChild size="lg" className="button rounded-full">
+          <Link href="/sign-in">Get Tickets</Link>
+        </Button>
+      </SignedOut>
+
+      <SignedIn>
+        {/* <Checkout event={event} userId={userId} /> */}
+        Checkout
+      </SignedIn>
+    </div>
+  );
 };
 
 export default CheckoutButton;
