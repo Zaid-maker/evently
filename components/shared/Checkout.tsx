a48d8c317b08cbf42a3120405829c53e9b34ce83
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-07-28 09:30:20 +0000

    update Checkout

--- components/shared/Checkout.tsx
+++ components/shared/Checkout.tsx
@@ -1,12 +1,24 @@
+import { checkoutOrder } from "@/lib/actions/order.actions";
 import { IEvent } from "@/lib/database/models/event.model";
-import React from "react";
 import { Button } from "../ui/button";
 
 const Checkout = ({ event, userId }: { event: IEvent; userId: string }) => {
+  const onCheckout = async () => {
+    const order = {
+      eventTitle: event.title,
+      eventId: event._id,
+      price: event.price,
+      isFree: event.isFree,
+      buyerId: userId,
+    };
+
+    await checkoutOrder(order);
+  };
+
   return (
-    <form method="post">
+    <form action={onCheckout} method="post">
       <Button type="submit" role="link" size="lg" className="button sm:w-fit">
-        Get Tickets
+        {event.isFree ? "Get Tickets" : "Buy Tickets"}
       </Button>
     </form>
   );
