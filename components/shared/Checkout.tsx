d7eb7404ce7b10c3fa862d79173033afb2ccc37b
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2024-07-28 09:37:15 +0000

    useEffect to check the status of checkout order

--- components/shared/Checkout.tsx
+++ components/shared/Checkout.tsx
@@ -1,8 +1,22 @@
 import { checkoutOrder } from "@/lib/actions/order.actions";
 import { IEvent } from "@/lib/database/models/event.model";
 import { Button } from "../ui/button";
+import { useEffect } from "react";
 
 const Checkout = ({ event, userId }: { event: IEvent; userId: string }) => {
+  useEffect(() => {
+    const query = new URLSearchParams(window.location.search);
+    if (query.get("success")) {
+      console.log("Order placed! You will receive an email confirmation.");
+    }
+
+    if (query.get("canceled")) {
+      console.log(
+        "Order canceled -- continue to shop around and checkout when you’re ready.",
+      );
+    }
+  }, []);
+
   const onCheckout = async () => {
     const order = {
       eventTitle: event.title,
